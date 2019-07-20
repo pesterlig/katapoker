d100075da0d80992f6204455129f1a299c659c05
pesterlig <pesterlig@gmail.com>
2019-07-20 10:04:31 -0400

    string split

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Hand {
 
-    public String listCards(String text){
-        return text;
+    public Hand() {
+    }
+
+    public String[] listCards(String text){
+        String[] cards = text.split(" ");
+        return cards;
     }
 
 }
--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -1,14 +1,45 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
+
 import static junit.framework.TestCase.*;
 
 public class HandTest {
 
-    @Test
-    public void whenHandIsPassedAStringItReturnsThatString(){
-        Hand hand = new Hand();
+    Hand hand;
 
+
+    @Before
+    public void setUp() {
+
+        hand = new Hand();
+    }
+
+    /*@Test
+    public void whenHandIsPassedAStringItReturnsThatString(){
         assertEquals("sometext",hand.listCards("sometext"));
     }
+*/
+    @Test
+    public void whenHandIsPassedAStringItReturnsAStringArray() {
+
+        String text = "Black White";
+        hand.listCards(text);
+        assertEquals("Black", hand.listCards(text)[0]);
+        assertEquals("White", hand.listCards(text)[1]);
+    }
+
+    /* @Test
+    public void whenHandIsPassedASampleInputItReturnsAStringArrayOfCardForBlack() {
+
+        String text = "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH"
+        hand.listCards(text);
+        assertEquals("Black", hand.listCards(text)[0]);
+        assertEquals("2H", hand.listCards(text)[13]);
+
+    }
+*/
+
 }
